e69beabb9184fa762ea814c831c0a24d457ad093
hp-e <hpephoto@gmail.com>
2019-11-01 09:30:09 +0100

    add database module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'labs', loadChildren: () => import('./lab/lab.module').then(m => m.LabModule) }];
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'labs', loadChildren: () => import('./lab/lab.module').then(m => m.LabModule) }, { path: 'database', loadChildren: () => import('./database/database.module').then(m => m.DatabaseModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
