cdf9beb5bcf5b72caab361452ac885a5e10d757c
hp-e <hpephoto@gmail.com>
2019-11-01 09:33:50 +0100

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'labs', loadChildren: () => import('./lab/lab.module').then(m => m.LabModule) }, { path: 'database', loadChildren: () => import('./database/database.module').then(m => m.DatabaseModule) }, { path: 'orders', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) }];
+const routes: Routes = [
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("./dashboard/dashboard.module").then(m => m.DashboardModule)
+  },
+  {
+    path: "labs",
+    loadChildren: () => import("./lab/lab.module").then(m => m.LabModule)
+  },
+  {
+    path: "database",
+    loadChildren: () =>
+      import("./database/database.module").then(m => m.DatabaseModule)
+  },
+  {
+    path: "orders",
+    loadChildren: () => import("./order/order.module").then(m => m.OrderModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
