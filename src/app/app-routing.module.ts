c0819c2f12ad0122245595edcd1b3b78bf4af784
hp-e <hpephoto@gmail.com>
2019-11-01 09:29:25 +0100

    add lab module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }];
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'labs', loadChildren: () => import('./lab/lab.module').then(m => m.LabModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
